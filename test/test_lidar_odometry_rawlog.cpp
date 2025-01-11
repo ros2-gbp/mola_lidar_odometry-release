adfdaa19bed943e4f81228b9b865a19c7f386cd1
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-01-11 11:45:47 +0100

    Rebase from 'release/jazzy/mola_lidar_odometry'

--- test/test_lidar_odometry_rawlog.cpp
+++ test/test_lidar_odometry_rawlog.cpp
@@ -65,7 +65,7 @@ int main_odometry(
   // Initialize estimator (default settings):
   stateEstimator->setMinLoggingLevel(mrpt::system::LVL_DEBUG);
   const auto cfgEstimator = mola::load_yaml_file(stateEstimConfigFile);
-  stateEstimator->initialize(cfgEstimator["params"]);
+  stateEstimator->initialize(cfgEstimator);
 
   // dataset input:
   mrpt::obs::CRawlog dataset;
@@ -100,7 +100,6 @@ int main_odometry(
   const bool gtLoadOk = gt.loadFromTextFile_TUM(gtTrajectory);
   ASSERT_(gtLoadOk);
 
-  ASSERT_EQUAL_(trajectory.size(), 3U);
   ASSERT_EQUAL_(gt.size(), trajectory.size());
 
   auto itP = trajectory.cbegin();
