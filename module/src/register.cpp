0fcf1d11d76b36f68ab6552f3fa6938a312b63cd
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2026-04-02 23:18:31 +0200

    Rebase from 'debian/rolling/mola_lidar_odometry'

--- module/src/register.cpp
+++ module/src/register.cpp
@@ -4,7 +4,7 @@
 | | | | | | (_) | | (_| | Localization and mApping (MOLA)
 |_| |_| |_|\___/|_|\__,_| https://github.com/MOLAorg/mola
 
- Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria,
+ Copyright (C) 2018-2026 Jose Luis Blanco, University of Almeria,
                          and individual contributors.
  SPDX-License-Identifier: GPL-3.0
  See LICENSE for full license information.
@@ -28,7 +28,7 @@
 #include <mrpt/core/initializer.h>
 #include <mrpt/rtti/CObject.h>
 
-MRPT_INITIALIZER(do_register_mola_lidar_odometry)
+MRPT_INITIALIZER(do_register_mola_lidar_odometry)  // NOLINT
 {
   using mrpt::rtti::registerClass;
 
